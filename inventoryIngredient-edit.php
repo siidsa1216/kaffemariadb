bcf27af65f794470e7b79edc67f3eca6042df8d4
maristelaa <angiemaristela71@gmail.com>
2022-06-16 23:56:03 +0800

    Update inventoryIngredient-edit.php

--- inventoryIngredient-edit.php
+++ inventoryIngredient-edit.php
@@ -1,6 +1,6 @@
 <?php
- require 'condb.php';
- 
+require 'condb.php';
+
 $ingredient_name="";
 $ingredient_description="";
 $ingredient_price="";
@@ -9,40 +9,61 @@ $ingredient_expiry="";
 $errorMessage = "";
 $successMessage = "";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(!isset($_GET['ingredient_ID'])){
+        header("location:/kaffemariadb/home.php");
+        exit;
+    }
+
+    $ingredient_ID = $_GET['ingredient_ID'];
+
+    $sql = "SELECT * FROM ingredient WHERE ingredient_ID=$ingredient_ID";
+    $result= $connection->query($sql);
+    $row = $result->fetch_assoc();
+
+    if(!$row){
+        header("location:/kaffemariadb/home.php");
+        exit;
+    }
+
+    $ingredient_name=$row["ingredient_name"];
+    $ingredient_description=$row["ingredient_description"];
+    $ingredient_price=$row["ingredient_price"];
+    $ingredient_expiry=$row["ingredient_expiry"];
+
+}
+else{
     $ingredient_name=$_POST["ingredient_name"];
     $ingredient_description=$_POST["ingredient_description"];
     $ingredient_price=$_POST["ingredient_price"];
     $ingredient_expiry=$_POST["ingredient_expiry"];
-    
+
     do {
         if(empty($ingredient_name) || empty($ingredient_description) || empty($ingredient_price) || empty($ingredient_expiry)) 
         {
             $errorMessage = "Please fill up the required fields";
         break;        
         } 
-        // add new ingredient into the db
-        $sql = "INSERT INTO ingredient (ingredient_name,ingredient_description,ingredient_price,ingredient_expiry)".
-                "VALUES('$ingredient_name','$ingredient_description','$ingredient_price', '$ingredient_expiry')";
-       
+        
+        $ingredient_ID = $_GET['ingredient_ID'];
+
+        $sql = "UPDATE ingredient ".
+        "SET ingredient_name = '$ingredient_name', ingredient_description = '$ingredient_description',  ingredient_price= '$ingredient_price', ingredient_expiry='$ingredient_expiry' ".
+        "WHERE ingredient_ID=$ingredient_ID ";
+        
         $result= $connection->query($sql);
 
-        if (!$result){
-            $errorMessage= "Invalid query: ".$connection->error;
-            break;
-        }
+        if(!$result) 
+        {
+            $errorMessage ="Invalid query: ". $connection->error;
+            break;      
+        } 
+        $successMessage = "Ingredient updated successfully!";
 
-        $ingredient_name="";
-        $ingredient_description="";
-        $ingredient_price="";
-        $ingredient_expiry="";
-
-        $successMessage = "Ingredient added successfully!";
-        
         header("location:/kaffemariadb/inventory.php");
         exit;
-
     }while(false);
+
 }
 ?>
 
@@ -54,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title>My Shop</title>
+    <title></title>
 </head>
 <body>
     <div class="container my-5">
@@ -71,6 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         ?>
         <form method = 'POST'>
+            <input type="hidden" name="ingredient_ID" value="<?php echo $ingredient_ID; ?>">
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Ingredient Name</label>
                 <div class="col-sm-6">
@@ -110,7 +132,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
                 <div class="col-sm-3 d-grid">
-                    <a class="btn btn-outline-primary" href="/kaffemariadb/inventory.php" role="button">Cancel</button></a>
+                    <a class="btn btn-outline-primary" href="/kaffemariadb/home.php" role="button">Cancel</a>
                 </div>
             </div>  
         </form>  
