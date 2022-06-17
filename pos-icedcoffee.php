0b618c81a399ffb850a4587fdcd42f07dd1f8705
siidsa1216 <jotsillador18@gmail.com>
2022-06-17 23:43:56 +0800

    iced-coffeee

--- pos-icedcoffee.php
+++ pos-icedcoffee.php
@@ -4,3 +4,72 @@ include 'include/pos-topbar.html';
 include 'include/pos-head.html';
 include 'include/pos-sidebar.html';
 ?>
+
+<body>
+<div class="bigside p-4">
+        <div class="container-fluid mt-5">
+                <div class="row">
+                <div class="col-lg-6">
+                    <div class="container-fluid p-3">
+                            <div class="row m-3">
+                                <div class="col-lg-4">
+                                    <div class="coffee">
+                                        <a href="/kaffemariadb/home.php">
+                                        <img src="/kaffemariadb/img/hot-coffee.jpg" style="width:150px; height:150px;"></a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="coffee">
+                                        <a href="/kaffemariadb/home.php">
+                                        <img src="/kaffemariadb/img/hot-coffee.jpg" style="width:150px; height:150px;"></a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="coffee">
+                                        <a href="/kaffemariadb/home.php">
+                                        <img src="/kaffemariadb/img/hot-coffee.jpg" style="width:150px; height:150px;"></a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="row m-3">
+                                <div class="col-lg-4">
+                                    <div class="coffee">
+                                        <a href="/kaffemariadb/home.php">
+                                        <img src="/kaffemariadb/img/hot-coffee.jpg" style="width:150px; height:150px;"></a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="coffee">
+                                        <a href="/kaffemariadb/home.php">
+                                        <img src="/kaffemariadb/img/hot-coffee.jpg" style="width:150px; height:150px;"></a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="coffee">
+                                        <a href="/kaffemariadb/home.php">
+                                        <img src="/kaffemariadb/img/hot-coffee.jpg" style="width:150px; height:150px;"></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+            
+                <div class="col-lg-6">
+                    <div class="container bg-white p-3">
+                    <table class="table text-center">
+                        <thead>
+                            <th>Beverage Name</th>
+                            <th>Beverage Flavor</th>
+                            <th>Beverage Quantity</th>
+                            <th>Beverage Price</th>
+                        </thead>
+                    </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+</body>
+</html>
