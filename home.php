4c7be39cf5a6f54866b90e3add8ff75968eb0046
violet <106017301+maristelaa@users.noreply.github.com>
2022-06-17 22:35:41 +0800

    Update home.php

--- home.php
+++ home.php
@@ -12,13 +12,43 @@ include 'include/head.html';
         </div>
             <div class="container-fluid mt-3">
                 <div class="row">
-                    <div class="col p-3 m-3 bg-primary text-white">BEVERAGE <br><br>(No. of Beverages Available)</div>
-                    <div class="col p-3 m-3 bg-dark text-white">INVENTORY <br><br>(No. of Beverages Available)</div>
-                    <div class="col p-3 m-3 bg-primary text-white">ITEMS <br><br>(No. of Beverages Available)</div>
+                    <div class="col p-3 m-3 bg-primary text-white">BEVERAGE <br><br><?php 
+                        $query = "SELECT COUNT(*) FROM beverage WHERE beverage_No=beverage_No";
+                        $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "$row[0]";
+                          }
+                        ?> Record(s)</div>
+                    <div class="col p-3 m-3 bg-dark text-white">INGREDIENT <br><br><?php 
+                        $query = "SELECT COUNT(*) FROM ingredient WHERE ingredient_ID=ingredient_ID";
+                        $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "$row[0]";
+                          }
+                        ?> Record(s)</div>
+                    <div class="col p-3 m-3 bg-primary text-white">ITEMS <br><br><?php 
+                        $query = "SELECT COUNT(*) FROM item WHERE item_No=item_No";
+                        $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "$row[0]";
+                          }
+                        ?> Record(s)</div>
                 </div>
                 <div class="row">
-                <div class="col p-3 m-3 bg-primary text-white">EMPLOYEE <br><br>(No. of Beverages Available)</div>
-                    <div class="col p-3 m-3 bg-dark text-white">SUPPLIERS <br><br>(No. of Beverages Available)</div>
+                <div class="col p-3 m-3 bg-primary text-white">STAFF <br><br><?php 
+                        $query = "SELECT COUNT(*) FROM staff WHERE staffID=staffID";
+                        $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "$row[0]";
+                          }
+                        ?> Record(s)</div>
+                    <div class="col p-3 m-3 bg-dark text-white">SUPPLIERS <br><br><?php 
+                        $query = "SELECT COUNT(*) FROM supplier WHERE supplier_ID=supplier_ID";
+                        $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "$row[0]";
+                          }
+                        ?> Record(s)</div>
                     <div class="col p-3 m-3 bg-primary text-white">SALES <br><br>(No. of Beverages Available)</div>
                 </div>
             </div>
