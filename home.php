72cedfcd6adcd005ff775779e761d25faff2972a
maristelaa <angiemaristela71@gmail.com>
2022-06-20 22:57:58 +0800

    Update home.php

--- home.php
+++ home.php
@@ -50,6 +50,13 @@ include 'include/head.html';
                           }
                         ?> Record(s)</div>
                     <div class="col p-3 m-3 bg-primary text-white">SALES <br><br>(No. of Beverages Available)</div>
+                    <div class="col p-3 m-3 bg-dark  text-white">NUMBER OF USERS <br><br><?php 
+                        $query = "SELECT COUNT(*) FROM user WHERE id=id";
+                        $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "$row[0]";
+                          }
+                        ?> Record(s)</div>
                 </div>
             </div>
      
