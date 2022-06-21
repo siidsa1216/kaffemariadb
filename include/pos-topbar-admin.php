e5b2e0e968ccb74b89918a95fc985daff641ad6d
maristelaa <angiemaristela71@gmail.com>
2022-06-21 14:45:13 +0800

    Create pos-topbar-admin.php

---
+++ include/pos-topbar-admin.php
@@ -0,0 +1,24 @@
+
+
+<div class="topbar" style="height: 100px;">
+    <nav class="navbar navbar-expand-sm text-dark text-center ">
+        <div class="container-fluid m-2">
+        <a class="logotxt" href="home.php">KAFFE MARIA POS AND INVENTORY SYSTEM</a>
+        </div>
+        <div class="container-fluid col-2 ml-2">
+            <div class="row">
+                <div class="col-9 mt-4">
+                    <h5 style="margin-left:-15px;"><?php session_start();
+                    echo "".$_SESSION['user'];?></h5>
+              </div>
+                <div class="col-3 mt-1">
+                    <a class="navbar-brand" href="#">
+                        <img src="img\favicon.ico" class="img-profile rounded-circle" style="margin-left: -25px;"> 
+                      </a>
+                </div>
+                <!--display the name of the logged in admin-->
+              
+            </div>
+        </div>
+      </nav>
+</div>
