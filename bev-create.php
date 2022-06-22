bc30e76a342a69066274491539fdbace291c8ea4
Jerbax23 <sanchez.joervy@gmail.com>
2022-06-21 19:55:33 -0700

    New ones

--- bev-create.php
+++ bev-create.php
@@ -2,7 +2,7 @@
  require 'condb.php';
  
  $beverage_No="";
- $beverage_flavor="";
+ $beverage_name="";
  $beverage_qty="";
  $beverage_size="";
  $beverage_price="";
@@ -14,7 +14,7 @@ $successMessage = "";
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $beverage_flavor=$_POST["beverage_flavor"];
+    $beverage_name=$_POST["beverage_name"];
     $beverage_qty=$_POST["beverage_qty"];
     $beverage_size=$_POST["beverage_size"];
     $beverage_price=$_POST["beverage_price"];
@@ -22,31 +22,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     
     do {
-        if(empty($beverage_flavor) || empty($beverage_qty) || empty($beverage_size) || empty($beverage_price) || empty($payment_method)) 
+        if(empty($beverage_name) || empty($beverage_qty) || empty($beverage_size) || empty($beverage_price) || empty($payment_method)) 
         {
             $errorMessage = "Please fill up the required fields";
         break;        
         } 
         // add new client into the db
-        $sql = "INSERT INTO beverage (beverage_flavor,beverage_qty,beverage_size,beverage_price,payment_method)".
-                "VALUES('$beverage_flavor', '$beverage_qty','$beverage_size','$beverage_price', '$payment_method')";
-       
-        $result= $connection->query($sql);
+        mysqli_query($connection, "INSERT INTO beverage VALUES(NULL, '$beverage_name', '$beverage_qty','$beverage_size','$beverage_price', '$payment_method')");
+        
+        $count=0;
+        $sql=mysqli_query($connection, "SELECT * FROM stock_master WHERE product_name='$beverage_name' && product_size='$beverage_size' ");
+        $count=mysqli_num_rows($sql);
 
-        if (!$result){
-            $errorMessage= "Invalid query: ".$connection->error;
-            break;
+        if($count==0){
+            mysqli_query($connection, "INSERT INTO stock_master VALUES(NULL, '$beverage_name', '$beverage_size', '$beverage_qty', '0')") or die(mysqli_error($connection));
+        }
+        else{
+            mysqli_query($connection, "UPDATE stock_master SET product_qty=product_qty+$beverage_qty WHERE product_name='$beverage_name' && product_size='$beverage_size' ") or die(mysqli_error($connection));
         }
 
-      
         $beverage_No="";
-        $beverage_flavor="";
+        $beverage_name="";
         $beverage_qty="";
         $beverage_size="";
         $beverage_price="";
         $payment_method="";
         
-        $successMessage = "Supplier added successfully!";
+        $successMessage = "Added successfully!";
         
         header("location:/kaffemariadb/bev.php");
         exit;
@@ -54,5 +56,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     }while(false);
 }
 ?>
+
 
 
